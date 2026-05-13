5f820c9d47fe23381ce14c0d7e740c4d93aaf353
Rasmus <dahlqvistrasmus@gmail.com>
2026-05-14 01:13:43 +0300

    updating matchticker

---
+++ data/teams.js
@@ -0,0 +1,64 @@
+const teams = [
+  {
+    name: "FC Inter",
+    shortName: "INT",
+    rating: 3
+  },
+  {
+    name: "AC Oulu",
+    shortName: "OUL",
+    rating: 3
+  },
+  {
+    name: "TPS",
+    shortName: "TPS",
+    rating: 3
+  },
+  {
+    name: "KuPS",
+    shortName: "KUPS",
+    rating: 3
+  },
+
+  {
+    name: "HJK",
+    shortName: "HJK",
+    rating: 2
+  },
+  {
+    name: "VPS",
+    shortName: "VPS",
+    rating: 2
+  },
+  {
+    name: "FC Lahti",
+    shortName: "LAHTI",
+    rating: 2
+  },
+  {
+    name: "SJK",
+    shortName: "SJK",
+    rating: 2
+  },
+
+  {
+    name: "Ilves",
+    shortName: "ILV",
+    rating: 1
+  },
+  {
+    name: "Jaro",
+    shortName: "JARO",
+    rating: 1
+  },
+  {
+    name: "Gnistan",
+    shortName: "GNS",
+    rating: 1
+  },
+  {
+    name: "IFK Mariehamn",
+    shortName: "MAR",
+    rating: 1
+  }
+];
--- frontend/app.js
+++ frontend/app.js
@@ -1,99 +1,143 @@
 console.log('Toimii')
-/**
- AC OuluFC Inter (Turku)
- FC LahtiFF Jaro (Pietarsaari)HJK (Helsinki)IF Gnistan
-  (Helsinki)IFK MariehamnIlves (Tampere)KuPS (Kuopio)SJK (Seinäjoki)TPS
-   (Turku)VPS (Vaasa)
- */
 
-const teams = [
-    {
-        name: "Ac Oulu",
-        rating: 3
-    },
-    {
-        name: "Fc Inter",
-        rating: 3
-    },
-    {
-        name: "Fc Lahti",
-        rating: 2
-    },
-    {
-        name: "IF Gnistan",
-        rating: 1
-    },
-    {
-        name: "FF Jaro",
-        rating: 1
-    },
-    {
-        name: "IFK Mariehamn",
-        rating: 1
-    },
-    {
-        name: "Ilves",
-        rating: 2
-    },
-    {
-        name: "SJK",
-        rating: 2
-    },
-    {
-        name: "TPS",
-        rating: 3
-    },
-    {
-        name: "Vps",
-        rating: 2
-    },
-    {
-        name: "HJK",
-        rating: 3
+
+
+
+const tickerContainer = document.getElementById("ticker-container");
+
+
+//const fixtureBox1 = document.createElement("div");
+const fixtureBox2 = document.createElement("div");
+const fixtureBox3 = document.createElement("div");
+const fixtureBox4 = document.createElement("div");
+const fixtureBox5 = document.createElement("div");
+
+const fixtureScore = 0; 
+
+
+const weekValue = document.getElementById("gameWeekSelecter");
+function getNextFixtures(teamName, limit) {
+    const emptyList = [];
+for(let i = 0; i < fixtures.length; i++) {
+    if(teamName ===  fixtures[i].hometeam) {
+        emptyList.push(fixtures[i].awayteam);
+        if(emptyList.length === limit) {
+            break;
+        }
     }
-
-]
-
-function checkRating() {
+     if(teamName ===  fixtures[i].awayteam) {
+        emptyList.push(fixtures[i].hometeam);
+        if(emptyList.length === limit) {
+            break;
+        }
+    }
     
+    
+   
+}
+emptyList.forEach(element => {
+        console.log(element)
+    });
+ return emptyList;
 }
 
+getNextFixtures("HJK", 3);
 
-const fixtures = [
-    { 
-    round:8,
-    hometeam: 'HJK',
-    awayteam: 'TPS'
-    },
-    { round:8,
-    hometeam: 'SJK',
-    awayteam: 'Fc Inter'
-    },
-    { round:8,
-    hometeam: 'AC Oulu',
-    awayteam: 'TPS'
-    },
-     { 
-    round:9,
-    hometeam: 'HJK',
-    awayteam: 'FF Jaro'
-    },
-    { round:9,
-    hometeam: 'TPS',
-    awayteam: 'Fc Inter'
-    },
-    { round:9,
-    hometeam: 'AC Oulu',
-    awayteam: 'SJK'
+
+
+function makeFixturerow(team) {
+    const teamRow1 = document.createElement("div");
+    const teamNameDiv = document.createElement("div");
+    teamRow1.classList.add("flex-container");
+    tickerContainer.appendChild(teamRow1);
+    teamRow1.appendChild(teamNameDiv);
+    teamNameDiv.innerText = team;
+    for(let i = 0; i < fixtures.length; i++) {
+    if(fixtures[i].hometeam === team) {
+         const fixtureBox1 = document.createElement("div");
+        fixtureBox1.innerText = fixtures[i].awayteam;
+        const teamRating = getTeamRating(fixtures[i].awayteam);
+        if(teamRating === 1) {
+            fixtureBox1.classList.add("difficulty-1");
+        }
+        if(teamRating === 2) {
+            fixtureBox1.classList.add("difficulty-2");
+        }
+        if(teamRating === 3) {
+            fixtureBox1.classList.add("difficulty-3");
+        }
+       
+        teamRow1.appendChild(fixtureBox1);
     }
-    
-]
+    if(fixtures[i].awayteam === team) {
+         const fixtureBox1 = document.createElement("div");
+        fixtureBox1.innerText = fixtures[i].hometeam;
+         const teamRating = getTeamRating(fixtures[i].hometeam);
+        if(teamRating === 1) {
+            fixtureBox1.classList.add("difficulty-1");
+        }
+        if(teamRating === 2) {
+            fixtureBox1.classList.add("difficulty-2");
+        }
+        if(teamRating === 3) {
+            fixtureBox1.classList.add("difficulty-3");
+        }
+        teamRow1.appendChild(fixtureBox1);
+    }
+   
+}
+}
 
-fixtures.forEach(element => {
-    console.log(element)
+function getTeamRating(teamName) {
+    for(let i = 0; i < teams.length; i++) {
+        if(teams[i].name === teamName) {
+           // console.log("Joukkueen rating: "+teams[i].rating)
+            return teams[i].rating;
+        }
+    }
+}
+
+teams.forEach(element => {
+    makeFixturerow(element.name)
 });
 
-const container = document.getElementById("fixtures")
+//getTeamRating("SJK");
+
+
+/*
+//HJK:n ottelut 
+for(let i = 0; i < fixtures.length; i++) {
+    if(fixtures[i].hometeam === "HJK") {
+         const fixtureBox1 = document.createElement("div");
+        fixtureBox1.innerText = fixtures[i].awayteam;
+        teamRow1.appendChild(fixtureBox1);
+    }
+    if(fixtures[i].awayteam === "HJK") {
+         const fixtureBox1 = document.createElement("div");
+        fixtureBox1.innerText = fixtures[i].hometeam;
+        teamRow1.appendChild(fixtureBox1);
+    }
+   
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function showAllMatches() {
     for(let i = 0; i < fixtures.length; i++) {
@@ -106,7 +150,7 @@ function showAllMatches() {
 }
 
 
-showAllMatches();
+//showAllMatches();
 function showSelectedMatches(number) {
 
 }
@@ -129,13 +173,33 @@ const showTeam = () => {
             for (let k = 0; k < teams.length; k++) {
                 if(teams[k].name === fixtures[i].hometeam) {
                 console.log("vastustajan rating: "+ teams[k].rating)
+                if(teams[k].rating === 1) {
+                    card.classList.add("difficulty-1");
+                    console.log("adding green");
+                }
+                if(teams[k].rating === 2) { 
+                     card.classList.add("difficulty-2"); 
+                }
+                 if(teams[k].rating === 3) {
+                     card.classList.add("difficulty-3"); 
+                }
                 }
              }
             }
         if(selectTeam.value != fixtures[i].awayteam) {
             for (let j = 0; j < teams.length; j++) {
                 if(teams[j].name === fixtures[i].awayteam) {
-                console.log("vastustajan rating: "+ teams[j].rating)
+                if(teams[j].rating === 1) {
+                   card.classList.add("difficulty-1");
+                   console.log("adding green");
+                }
+                if(teams[j].rating === 2) {
+                     card.classList.add("difficulty-2"); 
+                }
+                 if(teams[j].rating === 3) {
+                    card.classList.add("difficulty-3"); 
+                }
+                
                 }
              }
             }
